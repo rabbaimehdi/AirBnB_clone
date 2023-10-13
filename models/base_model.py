c1d88f7dcb4d909aaca19e93e685482ae3d8f09d
Mehdi <mehdi.rabbai@gmail.com>
2023-10-13 22:47:39 +0100

    Task 3 Basemodel done

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,23 @@ import datetime
 
 class BaseModel:
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
 
-    id = uuid.uuid4()
+    id = str(uuid.uuid4())
     created_at = datetime.datetime.now()
     updated_at = datetime.datetime.now()
 
     def __str__(self):
-        print(f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
     
     def to_dict(self):
-        dictionnary = self.__dict__
+        dictionnary = self.__dict__.copy()
         dictionnary["__class__"] = type(self).__name__
+        dictionnary['id'] = self.id
         dictionnary["created_at"] = self.created_at.isoformat()
         dictionnary["updated_at"] = self.updated_at.isoformat()
+        return dictionnary
