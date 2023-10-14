63c1e8d7a33e5727b8d648e07126434f460b43bb
Mehdi <mehdi.rabbai@gmail.com>
2023-10-14 01:31:57 +0100

    correction basemodel attributes

--- models/base_model.py
+++ models/base_model.py
@@ -5,15 +5,14 @@ import datetime
 #Write a class BaseModel that defines all common attributes/methods for other classes:
 
 class BaseModel:
-    
-    id = str(uuid.uuid4())
-    created_at = datetime.datetime.now()
-    updated_at = datetime.datetime.now()
+
 
     def __init__(self, *args, **kwargs):
         
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
-
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key =='created_at' or key =="updated_at":
