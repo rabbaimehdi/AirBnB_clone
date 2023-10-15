20b3f2d8497a10c558707e9a27f273eaf18ad2b5
Mehdi <mehdi.rabbai@gmail.com>
2023-10-15 19:04:52 +0100

    correct file_storage to check for class dict not instance dict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,35 +2,46 @@
 import json
 
 class FileStorage:
-    __file_path = '../../../file.json'
+    """Abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = 'file.json'
     __objects = {}
 
     def __init__(self):
         pass
 
     def all(self):
-        return self.__objects
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
     
     def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
         class_name = obj.__class__.__name__
         object_id = obj.id
-        self.__objects[f"{class_name}.{object_id}"] = obj
+        FileStorage.__objects[f"{class_name}.{object_id}"] = obj
 
     def save(self):
-        objects ={}
-        for key in self.__objects:
-            objects[key] = self.__objects[key].to_dict()
-            
-        with open(self.__file_path, 'w',) as file:
-            json.dump(objects, file)
+        """Serialize __objects to the JSON file __file_path."""
+        objects_dict = FileStorage.__objects
+        new_objects_dict = {}
+        for key in FileStorage.__objects:
+            new_objects_dict[key] = objects_dict[key].to_dict()
+
+        with open(FileStorage.__file_path, 'w',) as file:
+            json.dump(new_objects_dict, file)
 
     def reload(self):
+        """Deserialize the JSON file __file_path to __objects, no exception"""
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, 'r') as file:
                 dict = json.load(file)
             for item in dict.values():
                 class_name = item["__class__"]
-                # del item["__class__"]
+                del item["__class__"]
                 self.new(eval(class_name)(**item))
         except:
             pass
