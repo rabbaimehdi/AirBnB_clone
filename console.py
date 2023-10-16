cf93e5c973b285c1c2cb87f8c7e9af27592fe422
Mehdi <mehdi.rabbai@gmail.com>
2023-10-16 03:14:41 +0100

    add Console 0.0.1

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines the AiRBNB console."""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the AiRBNB command interpreter.
+
+    Attributes:
+        prompt (str): The command.
+    """
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing when an empty line."""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
