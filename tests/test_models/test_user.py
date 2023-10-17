3099ef8c6d049dd1a3c64dbe669ed2d54564cbf2
Mehdi <mehdi.rabbai@gmail.com>
2023-10-18 00:08:28 +0100

    add unittest for User

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Unittests for models/user.py.
+
+Unittest classes:
+    TestUser_instantiation
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.user import User
+
+
+class TestUser_instantiation(unittest.TestCase):
+    """Testing instantiation of the User class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(User, type(User()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(User(), models.storage.all().values())
+
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User.last_name))
+
+    def test_two_users_unique_ids(self):
+        us1 = User()
+        us2 = User()
+        self.assertNotEqual(us1.id, us2.id)
+
+if __name__ == "__main__":
+    unittest.main()
