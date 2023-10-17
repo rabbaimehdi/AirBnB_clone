63e902d027948407a62bf4a61491fdf745286d02
Mehdi <mehdi.rabbai@gmail.com>
2023-10-17 23:35:20 +0100

    add update in console.py

--- console.py
+++ console.py
@@ -77,19 +77,64 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """ Display string representations of all instances of a given class.
-        If no class is specified, displays all objects."""
+        If no class is specified, displays all objectsects."""
         arguments = parse(arg)
         if len(arguments) > 0 and arguments[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
-            objectslist = []
-            for obj in storage.all().values():
-                if len(arguments) > 0 and arguments[0] == obj.__class__.__name__:
-                    objectslist.append(obj.__str__())
+            objectsectslist = []
+            for objects in storage.all().values():
+                if len(arguments) > 0 and arguments[0] == objects.__class__.__name__:
+                    objectsectslist.append(objects.__str__())
                 elif len(arguments) == 0:
-                    objectslist.append(obj.__str__())
-            print(objectslist)
+                    objectsectslist.append(objects.__str__())
+            print(objectsectslist)
 
+    def do_update(self, arg):
+        """Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        arguments = parse(arg)
+        dictionary = storage.all()
+
+        if len(arguments) == 0:
+            print("** class name missing **")
+            return False
+        if arguments[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return False
+        if len(arguments) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(arguments[0], arguments[1]) not in dictionary.keys():
+            print("** no instance found **")
+            return False
+        if len(arguments) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(arguments) == 3:
+            try:
+                type(eval(arguments[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(arguments) == 4:
+            objects = dictionary["{}.{}".format(arguments[0], arguments[1])]
+            if arguments[2] in objects.__class__.__dict__.keys():
+                valuetype = type(objects.__class__.__dict__[arguments[2]])
+                objects.__dict__[arguments[2]] = valuetype(arguments[3])
+            else:
+                objects.__dict__[arguments[2]] = arguments[3]
+        elif type(eval(arguments[2])) == dict:
+            objects = dictionary["{}.{}".format(arguments[0], arguments[1])]
+            for k, v in eval(arguments[2]).items():
+                if (k in objects.__class__.__dict__.keys() and
+                        type(objects.__class__.__dict__[k]) in {str, int, float}):
+                    valuetype = type(objects.__class__.__dict__[k])
+                    objects.__dict__[k] = valuetype(v)
+                else:
+                    objects.__dict__[k] = v
+        storage.save()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
